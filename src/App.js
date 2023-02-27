2352d2e0f0297d1ed3649fb3c4351ba3a94560ab
antonkolesnikov <girrcen666@icloud.com>
2023-02-27 06:55:20 -0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Card from 'react-bootstrap/Card';
 import React, { useEffect, useState } from "react";
 import {getUrlVars, route} from "./routing/routing";
+import Header from "./header/js/header";
 import TableReq from "./table/TableInterdepartmentalRequests"
 import './App.css';
 
@@ -9,11 +10,14 @@ function App() {
     route(getUrlVars());
   };
   return (
-      <div >
-        <script type="text/javascript">route(getUrlVars());</script>
-        <Card.Body>
+      <div>
 
-        </Card.Body>
+          <script type="text/javascript">route(getUrlVars());</script>
+          <Header/>
+
+          <Card.Body>
+
+          </Card.Body>
 
         <Card.Footer></Card.Footer>
       </div>
---
+++ src/header/js/header.js
@@ -0,0 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../css/header.css'
+import {UpHome} from '../../routing/routing';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Home() {
+    function handleClick() {
+        UpHome();
+
+    }
+    return(<Nav.Link onClick={handleClick} href="#home">Главная</Nav.Link>);
+}
+
+function Brand() {
+    function handleClick() {
+        UpHome();
+    }
+    return(<Navbar.Brand onClick={handleClick} href="#home">Межведомственные запросы</Navbar.Brand>);
+    }
+function Header() {
+
+    return (
+        <Container className="my-container-header">
+            <Navbar  bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Brand/>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Home/>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            </Container>
+    );
+}
+
+export default Header;
